59967aef172018c574f4092dca07fd392a400ef0
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-06-10 02:25:56 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "" # 🤖 Get It From My.telegram.org.
