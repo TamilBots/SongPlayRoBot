ae88bc5474ae74016d42286ab3aeb49bd1349c52
TamilVenthan Mec <85955814+darkmanrandy@users.noreply.github.com>
2021-07-18 09:20:34 +0530

    Update SongPlayRoBot.py

--- SongPlayRoBot.py
+++ SongPlayRoBot.py
@@ -37,8 +37,8 @@ def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬', url='https://t.me/TamilSupport'),
-                    InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 🤗', url='https://t.me/SongPlayRoBot?startgroup=true')
+                    InlineKeyboardButton('𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬', url='https://t.me/Tharamaanamovies'),
+                    InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 🤗', url='https://t.me/youtubesongsdlbot?startgroup=true')
                 ]
             ]
         )
