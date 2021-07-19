560e0421bfbe213b79c127ac7f6f2872e194e60c
TamilVenthan Mec <85955814+darkmanrandy@users.noreply.github.com>
2021-07-19 09:09:19 +0530

    Update and rename SongPlayRoBot.py to AnySongDL.py

--- AnySongDL.py
+++ AnySongDL.py
@@ -38,7 +38,7 @@ def start(client, message):
             [
                 [
                     InlineKeyboardButton('𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬', url='https://t.me/Tharamaanamovies'),
-                    InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 🤗', url='https://t.me/youtubesongsdlbot?startgroup=true')
+                    InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 🤗', url='https://t.me/anysongdlrobot?startgroup=true')
                 ]
             ]
         )
