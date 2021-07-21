e688324db2c6c4b9d1aaadf80a94bd25ef525603
Jeeyaz_bot <87598600+Frozenkavin@users.noreply.github.com>
2021-07-21 07:19:41 +0530

    Update and rename config.py to frozenkavinconfig.py
    Music

---
+++ frozenkavinconfig.py
@@ -0,0 +1,4 @@
+class Config:
+    API_ID = "7457559"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "6bc24b0a199f520cfd2fefd1db12ce15" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1900786968:AAG-oUKeecvNztrYMkCBZx8CZ8Wp11luNWg" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
